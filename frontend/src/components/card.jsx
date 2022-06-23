b72e6e69ed9f026486dc5cb847a9d71911f9c90d
taeate <yes3144@nate.com>
2022-06-23 14:36:19 +0900

    list 목록 mui 의 card 를 이용하여 디자인

--- frontend/src/components/card.jsx
+++ frontend/src/components/card.jsx
@@ -21,7 +21,6 @@ export default function AreaCard() {
               species, ranging across all continents except Antarctica
               Lizards are a widespread group of squamate reptiles, with over 6,000
               species, ranging across all continents except Antarctica
-         
           </Typography>
             <div className='flex'>
               <div className="text-sm mt-4 text-blue-600">[0001]</div>
